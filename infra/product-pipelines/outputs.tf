47b2ae8832157908e15f2941a35166ea4bd9bb58
Peter Buckley <8352591+pbuckley@users.noreply.github.com>
2026-07-21 17:11:37 -0400

    Move demo collateral out of LLVM fork

--- infra/product-pipelines/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "product_pipelines" {
-  description = "Product pipeline slugs and GraphQL IDs."
-  value = {
-    for product, pipeline in buildkite_pipeline.product : product => {
-      id   = pipeline.id
-      slug = pipeline.slug
-    }
-  }
-}
